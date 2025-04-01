faf16717b246c499a6330fdec1838a9c1fa813cf
AAMIR AHMED <aamirahmed2132@gmail.com>
2025-04-01 22:00:47 +0000

    first commit

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className = ''bg-purple-200>
+            
+            
+        </nav>
+    );
+};
+
+export default Navbar;
